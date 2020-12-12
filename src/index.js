d9e5975d2d07409e39b4ff84e971fd84ef1b7191
Taylor Brown <taylor@davtar.org>
2020-12-11 22:08:56 -0500

    Initialize a template for index.js (This commit also acts as a test to see if my local branch and remote branch sync correctly...)

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <React.StrictMode>
+        {
+            // TODO: render app here
+        }
+    </React.StrictMode>,
+    document.getElementById('root')
+)
