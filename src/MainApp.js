dff4f013eff44554f13d29fb6e5e262d9921c2f3
Taylor Brown <taylor@davtar.org>
2020-12-30 11:08:22 -0500

    Implement MainApp

---
+++ src/MainApp.js
@@ -0,0 +1,12 @@
+import ReactStyles from 'react-styles'
+
+class MainApp extends ReactStyles.Component {
+    render() {
+        return <div>
+            <h1>Test Complete!</h1>
+            <p>The main app rendered successfully!</p>
+        </div>
+    }
+}
+
+export default MainApp
