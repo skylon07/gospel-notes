89f351646242595534515cdf67dab0e40bf5c0a2
Taylor Brown <taylor@davtar.org>
2020-12-31 16:51:45 -0500

    Create a MainWindow component (inside MainApp.js/.css)

--- src/MainApp/MainApp.js
+++ src/MainApp/MainApp.js
@@ -3,9 +3,17 @@ import './MainApp.css'
 
 class MainApp extends React.Component {
     render() {
-        return <div className="MainApp">
-            <h1>Test Complete!</h1>
-            <p>The main app rendered successfully!</p>
+        return <MainWindow>
+            This is the main window!
+        </MainWindow>
+    }
+}
+
+// a container that holds any settings/data pertaining to the window
+class MainWindow extends React.Component {
+    render() {
+        return <div className="MainWindow">
+            {this.props.children}
         </div>
     }
 }
