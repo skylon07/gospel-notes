ba5d695eb2064b4bcf3b94ec545f160d97563f96
Taylor Brown <taylor@davtar.org>
2021-01-07 20:49:46 -0500

    Split TextNode into a parent/child class

--- src/lib/text-fit/index.js
+++ src/lib/text-fit/index.js
@@ -6,19 +6,21 @@ fitting text into HTML elements.
 import React from 'react'
 import './styles.css'
 
-// a basic node that will resize its text to fit its parent
-export class TextNode extends React.Component {
+// an abstract class that defines similar TextNode behavior
+class TextNode extends React.Component {
     constructor(props) {
         super(props)
 
         // refs used for detecting width/height targets and setting font sizes
         this.selfRef = React.createRef()
         this.textRef = React.createRef()
+
+        this.classExtension = ""
     }
 
     render() {
         // NOTE: children should be a raw string of text
-        return <div ref={this.selfRef} className="TextNode">
+        return <div ref={this.selfRef} className={`TextNode ${this.classExtension}`}>
             <div ref={this.textRef} className="textSizer">
                 {this.props.children}
             </div>
@@ -26,26 +28,15 @@ export class TextNode extends React.Component {
     }
 
     componentDidMount() {
-        const self = this.selfRef.current
-        let lastWidth, lastHeight // undefined !== number; runs update on first frame
-        // records this._interval for unmount; do not rely on this value existing!
-        const intervalFunc = () => this._interval = requestAnimationFrame(() => {
-           if (lastWidth !== self.clientWidth || lastHeight !== self.clientHeight) {
-                this._updateTextSize()
-                lastWidth = self.clientWidth
-                lastHeight = self.clientHeight
-            }
-
-            intervalFunc()
-        })
-        intervalFunc()
+        this.onMount()
     }
 
     componentWillUnmount() {
-        cancelAnimationFrame(this._interval)
+        this.onUnmount()
     }
 
-    _updateTextSize() {
+    // iterates and fits text into the parent container, returning the final font size
+    updateTextSize() {
         const selfElem = this.selfRef.current
         const maxWidth = selfElem.clientWidth
         // NOTE: height is also used to allow wrapping text
@@ -56,17 +47,17 @@ export class TextNode extends React.Component {
         const text = this.props.children
         const willBreak = text.includes(' ') // TODO: do more extensive "will break" testing (commas? tabs?)
         if (typeof text === "string" && willBreak) {
-            this._updateTextSizeWrap(maxWidth, maxHeight)
+            return this._updateTextSizeWrap(maxWidth, maxHeight)
         }
         else {
-            this._updateTextSizeNoWrap(maxWidth, maxHeight)
+            return this._updateTextSizeNoWrap(maxWidth, maxHeight)
         }
     }
 
     // the default, "NoWrap" version of the updating algorithm (ie single word text)
     _updateTextSizeNoWrap(maxWidth, maxHeight) {
         // we want to always start at zero (this avoids any potential syncing issues with relative changes)
-        let currDims = this._setFontSize(0)
+        let currDims = this.setFontSize(0)
         // NOTE: this updates in increments of 144, 29, 6, and 1 as a searching optimization
         // (these numbers were chosen based on this logic:
         //   - we want to add 1 a max of 5 times; 6's moduar range is 5 (6 % x == 5 at highest)
@@ -77,30 +68,32 @@ export class TextNode extends React.Component {
         //     ensures we will not be iterating/resizing more than 22 times
         // )
         while (currDims.width < maxWidth && currDims.height < maxHeight) {
-            currDims = this._setFontSize(currDims.fontSize + 144)
+            currDims = this.setFontSize(currDims.fontSize + 144)
         }
 
         // operators reversed since width/height is now larger than target
         while (currDims.width > maxWidth || currDims.height > maxHeight) {
-            currDims = this._setFontSize(currDims.fontSize - 29)
+            currDims = this.setFontSize(currDims.fontSize - 29)
         }
 
         // reversed again for the same reason
         while (currDims.width < maxWidth && currDims.height < maxHeight) {
-            currDims = this._setFontSize(currDims.fontSize + 6)
+            currDims = this.setFontSize(currDims.fontSize + 6)
         }
 
         // having an even number of loops ensures we end up underneath target/max value
         while (currDims.width > maxWidth || currDims.height > maxHeight) {
-            currDims = this._setFontSize(currDims.fontSize - 1)
+            currDims = this.setFontSize(currDims.fontSize - 1)
         }
+
+        return currDims.fontSize
     }
 
     // the alternate, "Wrap" version of the updating algorithm (ie multiple word text)
     _updateTextSizeWrap(maxWidth, maxHeight) {
-        let currDims = this._setFontSize(0)
+        let currDims = this.setFontSize(0)
         while (currDims.width < maxWidth && currDims.height < maxHeight) {
-            currDims = this._setFontSize(currDims.fontSize + 144)
+            currDims = this.setFontSize(currDims.fontSize + 144)
         }
         // NOTE: since wrapping text trades width for height, sometimes (when the width
         // is greater than the height) an element will wrap text without trading all the
@@ -108,28 +101,30 @@ export class TextNode extends React.Component {
         // if the width changes, and only stop when it does
         let lastWidth = currDims.width
         while (lastWidth === currDims.width && currDims.height < maxHeight) {
-            currDims = this._setFontSize(currDims.fontSize + 144)
+            currDims = this.setFontSize(currDims.fontSize + 144)
         }
 
         while (currDims.width > maxWidth || currDims.height > maxHeight) {
-            currDims = this._setFontSize(currDims.fontSize - 29)
+            currDims = this.setFontSize(currDims.fontSize - 29)
         }
 
         while (currDims.width < maxWidth && currDims.height < maxHeight) {
-            currDims = this._setFontSize(currDims.fontSize + 6)
+            currDims = this.setFontSize(currDims.fontSize + 6)
         }
         // extra comparison is also added here
         lastWidth = currDims.width
         while (lastWidth === currDims.width && currDims.height < maxHeight) {
-            currDims = this._setFontSize(currDims.fontSize + 6)
+            currDims = this.setFontSize(currDims.fontSize + 6)
         }
 
         while (currDims.width > maxWidth || currDims.height > maxHeight) {
-            currDims = this._setFontSize(currDims.fontSize - 1)
+            currDims = this.setFontSize(currDims.fontSize - 1)
         }
+
+        return currDims.fontSize
     }
 
-    _setFontSize(size) {
+    setFontSize(size) {
         if (size < 0) {
             size = 0 // don't want any CSS errors with negative font sizes!
         }
@@ -138,5 +133,35 @@ export class TextNode extends React.Component {
         textElem.style['font-size'] = `${size}px`
 
         return { fontSize: size, width: textElem.clientWidth, height: textElem.clientHeight }
+    }
+}
+
+// a basic node that will resize its text continually to fit its parent
+// NOTE: this works fine in small groups, but don't put more than a few of these in your app;
+// if you need large numbers, use the faster StaticTextNode instead
+export class ConstantTextNode extends TextNode {
+    constructor(props) {
+        super(props)
+        this.classExtension = "constant"
+    }
+
+    onMount() {
+        const self = this.selfRef.current
+        let lastWidth, lastHeight // undefined !== number; runs update on first frame
+        // records this._interval for unmount; do not rely on this value existing!
+        const intervalFunc = () => this._interval = requestAnimationFrame(() => {
+            if (lastWidth !== self.clientWidth || lastHeight !== self.clientHeight) {
+                this.updateTextSize()
+                lastWidth = self.clientWidth
+                lastHeight = self.clientHeight
+            }
+
+            intervalFunc()
+        })
+        intervalFunc()
+    }
+
+    onUnmount() {
+        cancelAnimationFrame(this._interval)
     }
 }
