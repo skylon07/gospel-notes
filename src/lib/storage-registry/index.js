d812cffde7963d06479453284255c1896c1d9cad
David M. Brown <david@davtar.org>
2021-03-09 23:37:04 -0700

    Formatting cleanup while adding unit tests

--- src/lib/storage-registry/index.js
+++ src/lib/storage-registry/index.js
@@ -1,38 +1,42 @@
 class RegistryCreationError extends Error {
     constructor(msg) {
-        super(msg)
-        this.name = "RegistryCreationError"
+        super(msg);
+        this.name = "RegistryCreationError";
     }
 }
 
 class RegistryStoreError extends Error {
     constructor(msg) {
-        super(msg)
-        this.name = "RegistryStoreError"
+        super(msg);
+        this.name = "RegistryStoreError";
     }
 }
 
 // a dictionary of parsing separators
 const SEPARATORS = {
-    item: '≡' + ';' + '≡', // using '+' allows loading this file without confusing the parser 
-    pair: '≡' + ',' + '≡',
-}
+    // item: '≡' + ';' + '≡', // using '+' allows loading this file without confusing the parser
+    // pair: '≡' + ',' + '≡',
+    item: "≡;≡",
+    pair: "≡,≡",
+};
 
 // a registry for localStorage management
 export class StorageRegistry {
     static DANGEROUS_clearStorage() {
-        localStorage.clear()
+        localStorage.clear();
     }
 
     constructor(storageKey) {
         if (!storageKey || typeof storageKey !== "string") {
-            throw new RegistryCreationError(`StorageRegistry must be created with a non-null string (got '${storageKey}')`)
+            throw new RegistryCreationError(
+                `StorageRegistry must be created with a non-null string (got '${storageKey}')`
+            );
         }
 
-        this._storageKey = storageKey
-        this._updateQueued = false
-        this._reset = false
-        this._initStoredStrs()
+        this._storageKey = storageKey;
+        this._updateQueued = false;
+        this._reset = false;
+        this._initStoredStrs();
     }
 
     onStoreFinish() {
@@ -46,77 +50,73 @@ export class StorageRegistry {
     }
 
     get keys() {
-        let keys = []
-        for (let key in this._strs) {
-            keys.push(key)
-        }
-        return keys
+        return Object.keys(this._strs);
     }
 
     get empty() {
-        for (let key in this._strs) {
-            return false
-        }
-        return true
+        return this.numberKeys === 0;
     }
+
     get numberKeys() {
-        let n = 0
-        for (let key in this._strs) {
-            n++
-        }
-        return n
+        return this.keys.length;
     }
 
     getKey(key) {
-        const result = this._strs[key]
+        const result = this._strs[key];
         if (result === undefined) {
-            return null
+            return null;
         }
-        return result
+        return result;
     }
 
     // NOTE: key/str pair is stored immediately, but localStorage is updated asyncronously
     setKeyString(key, str) {
-        if (key === '') {
-            throw new RegistryStoreError(`Cannot set empty string ('') as storage key`)
+        if (key === "") {
+            throw new RegistryStoreError(
+                `Cannot set empty string ('') as storage key`
+            );
         }
         if (typeof str !== "string" && typeof str !== "number") {
             if (typeof str === "symbol") {
-                str = "Symbol()"
+                str = "Symbol()";
             }
-            throw new RegistryStoreError(`Cannot set storage key '${key}' to non-string/number '${str}'`)
+            throw new RegistryStoreError(
+                `Cannot set storage key '${key}' to non-string/number '${str}'`
+            );
         }
         if (str.includes(SEPARATORS.pair) || str.includes(SEPARATORS.item)) {
-            throw new RegistryStoreError(`Registry cannot store strings with reserved substrings '${SEPARATORS.pair}' or '${SEPARATORS.item}'`)
+            throw new RegistryStoreError(
+                `Registry cannot store strings with reserved substrings '${SEPARATORS.pair}' or '${SEPARATORS.item}'`
+            );
         }
 
-        this._strs[key] = str
-        this._requestUpdate()
+        this._strs[key] = str;
+        this._requestUpdate();
     }
 
     resetKey(key) {
-        delete this._strs[key]
-        this._requestUpdate()
+        delete this._strs[key];
+        this._requestUpdate();
     }
 
     reset(mode = "") {
-        this._strs = {}
+        this._strs = {};
         if (mode === "hard") {
-            localStorage.removeItem(this._storageKey)
+            localStorage.removeItem(this._storageKey);
         }
     }
 
     _initStoredStrs() {
-        const storageStr = localStorage[this._storageKey] || ""
-        this._strs = {}
+        const storageStr = localStorage[this._storageKey] || "";
+        this._strs = {};
 
         if (!storageStr) {
-            return
+            return;
         }
 
         for (let pair of storageStr.split(SEPARATORS.item)) {
-            let [key, str] = pair.split(SEPARATORS.pair)
-            this._strs[key] = str
+            let [key, str] = pair.split(SEPARATORS.pair);
+            this._strs[key] = str;
         }
     }
 
@@ -124,31 +124,29 @@ export class StorageRegistry {
         if (!this._updateQueued) {
             // delay is a security and optimization measure (mostly for async functions);
             // its less likely to save "crashing data" and batches more set requests together
-            setTimeout(() => this._updateStorage(), 100)
-            this._updateQueued = true
+            setTimeout(() => this._updateStorage(), 100);
+            this._updateQueued = true;
         }
     }
 
     _updateStorage() {
-        this._updateQueued = false
+        this._updateQueued = false;
 
-        let storageList = []
+        let storageList = [];
         for (const key in this._strs) {
-            const str = this._strs[key]
-            const pair = `${key}${SEPARATORS.pair}${str}`
-            storageList.push(pair)
+            const str = this._strs[key];
+            const pair = `${key}${SEPARATORS.pair}${str}`;
+            storageList.push(pair);
         }
 
         try {
-            localStorage[this._storageKey] = storageList.join(SEPARATORS.item)
-            this.onStoreFinish()
-        }
-        catch (error) {
+            localStorage[this._storageKey] = storageList.join(SEPARATORS.item);
+            this.onStoreFinish();
+        } catch (error) {
             if (error.name === "QuotaExceededError") {
-                this.onStoreFull()
-            }
-            else {
-                this.onStoreError(error)
+                this.onStoreFull();
+            } else {
+                this.onStoreError(error);
             }
         }
     }
