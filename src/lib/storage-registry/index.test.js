57456535c7f0fe4a7635f7206f82d8485d5b5d3f
Taylor Brown <taylor@davtar.org>
2021-03-11 19:48:25 -0500

    Change test() to it(), and add a few extra tests (This commit also reran npm install, since the tests failed due to a Jest reference error; therefore the lockfile was updated.)

--- src/lib/storage-registry/index.test.js
+++ src/lib/storage-registry/index.test.js
@@ -1,76 +1,91 @@
-import { StorageRegistry, RegistryStoreError } from ".";
+import { StorageRegistry, RegistryStoreError, SEPARATORS } from ".";
 
-describe("tests", () => {
-    let storage;
+let storage;
 
-    beforeEach(() => {
-        storage = new StorageRegistry("test");
-    });
+beforeEach(() => {
+    storage = new StorageRegistry("test");
+});
 
-    test("keys with zero keys", () => {
-        expect(storage.keys).toStrictEqual([]);
-    });
+it("initializes with keys === []", () => {
+    expect(storage.keys).toStrictEqual([]);
+});
 
-    test("keys with one key", () => {
-        storage.setKeyString("key", "val");
-        expect(storage.keys).toStrictEqual(["key"]);
-    });
+it("returns one key after one value is stored", () => {
+    storage.setKeyString("key", "val");
+    expect(storage.keys).toStrictEqual(["key"]);
+});
 
-    test("keys with two keys", () => {
-        storage.setKeyString("key", "val");
-        storage.setKeyString("key2", "val2");
-        expect(storage.keys).toIncludeSameMembers(["key", "key2"]);
-    });
+it("returns two keys after two values are stored", () => {
+    storage.setKeyString("key", "val");
+    storage.setKeyString("key2", "val2");
+    expect(storage.keys).toIncludeSameMembers(["key", "key2"]);
+});
 
-    test("numberKeys with zero keys", () => {
-        expect(storage.numberKeys).toBe(0);
-    });
+it("removes keys when they are reset", () => {
+    storage.setKeyString("key", "val");
+    storage.setKeyString("key2", "val2");
+    storage.resetKey("key");
+    expect(storage.keys).toIncludeSameMembers(["key2"]);
+});
 
-    test("numberKeys with one key", () => {
-        storage.setKeyString("key", "val");
-        expect(storage.numberKeys).toBe(1);
-    });
+it("initializes with numberKeys === 0", () => {
+    expect(storage.numberKeys).toBe(0);
+});
 
-    test("numberKeys with two keys", () => {
-        storage.setKeyString("key", "val");
-        storage.setKeyString("key2", "val2");
-        expect(storage.numberKeys).toBe(2);
-    });
+it("numbers one key after one value is stored", () => {
+    storage.setKeyString("key", "val");
+    expect(storage.numberKeys).toBe(1);
+});
 
-    test("empty when empty", () => {
-        expect(storage.empty).toBe(true);
-    });
+it("numbers two keys after two values are stored", () => {
+    storage.setKeyString("key", "val");
+    storage.setKeyString("key2", "val2");
+    expect(storage.numberKeys).toBe(2);
+});
 
-    test("empty when not empty", () => {
-        storage.setKeyString("key", "val");
-        expect(storage.empty).toBe(false);
-    });
+it("returns empty === true when no keys are stored", () => {
+    expect(storage.empty).toBe(true);
+});
 
-    test("getKey with non-existing key", () => {
-        expect(storage.getKey("key")).toBe(null);
-    });
+it("returns empty === true after keys are stored and deleted", () => {
+    storage.setKeyString("key", "val");
+    storage.setKeyString("key2", "val2");
+    storage.resetKey("key");
+    storage.resetKey("key2");
+    expect(storage.empty).toBe(true);
+});
 
-    test("getKey with existing key", () => {
-        storage.setKeyString("key", "val");
-        expect(storage.getKey("key")).toBe("val");
-    });
+it("returns empty === false when keys are stored", () => {
+    storage.setKeyString("key", "val");
+    storage.setKeyString("key2", "val2");
+    storage.resetKey("key");
+    expect(storage.empty).toBe(false);
+});
 
-    test("setKeyString with valid string doesn't throw", () => {
-        expect(storage.setKeyString("key", "val")).toBe(undefined);
-    });
+it("returns null when an undefined key is requested", () => {
+    expect(storage.getKey("key")).toBe(null);
+});
 
-    test("setKeyString with empty string throws", () => {
-        expect(() => {
-            storage.setKeyString("", "val");
-        }).toThrow(RegistryStoreError);
-    });
+it("returns the value after a key is stored and requested", () => {
+    storage.setKeyString("key", "val");
+    expect(storage.getKey("key")).toBe("val");
+});
 
-    test("setKeyString with separator string throws", () => {
-        expect(() => {
-            storage.setKeyString("key", "≡;≡");
-        }).toThrow(RegistryStoreError);
-        expect(() => {
-            storage.setKeyString("key", "≡,≡");
-        }).toThrow(RegistryStoreError);
-    });
+it("doesn't throw on valid setKeyString() calls", () => {
+    expect(storage.setKeyString("key", "val")).toBe(undefined);
+});
+
+it("throws an error when setting an empty key to a value", () => {
+    expect(() => {
+        storage.setKeyString("", "val");
+    }).toThrow(RegistryStoreError);
+});
+
+it("throws an error when setting a key to a reserved separator value", () => {
+    expect(() => {
+        storage.setKeyString("key", SEPARATORS.item);
+    }).toThrow(RegistryStoreError);
+    expect(() => {
+        storage.setKeyString("key", SEPARATORS.pair);
+    }).toThrow(RegistryStoreError);
 });
