e07e816ca753938611f0fe680181c8cd1df1daaa
Taylor Brown <taylor@davtar.org>
2021-03-14 20:40:54 -0400

    Add tests for the SearchBar

--- src/navigation/SearchBar.test.js
+++ src/navigation/SearchBar.test.js
@@ -96,3 +96,77 @@ it("selects all text in the input field when focused", () => {
     expect(inputField.selectionStart).toBe(0);
     expect(inputField.selectionEnd).toBe(searchString.length);
 });
+
+it("triggers onSearch() with correct value on search button click", () => {
+    const onSearch = jest.fn();
+    act(() => {
+        render(<SearchBar onSearch={onSearch} />, root);
+    });
+    const searchBar = grabSearchBar();
+    const inputField = searchBar.querySelector("input");
+    const searchButton = searchBar.querySelector(".SearchButton > button");
+
+    const searchString = "this is a test";
+    act(() => {
+        inputField.value = searchString;
+    });
+
+    expect(onSearch).not.toBeCalled();
+
+    act(() => {
+        searchButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+
+    expect(inputField.value).toBe(searchString);
+    expect(onSearch).toHaveBeenCalledTimes(1);
+    expect(onSearch).toHaveBeenLastCalledWith(searchString);
+
+    const searchString2 = "this is another test";
+    act(() => {
+        inputField.value = searchString2;
+        searchButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+
+    expect(inputField.value).toBe(searchString2);
+    expect(onSearch).toHaveBeenCalledTimes(2);
+    expect(onSearch).toHaveBeenLastCalledWith(searchString2);
+});
+
+it("triggers onSearch() when the enter key is pressed", () => {
+    const onSearch = jest.fn();
+    act(() => {
+        render(<SearchBar onSearch={onSearch} />, root);
+    });
+    const searchBar = grabSearchBar();
+    const inputField = searchBar.querySelector("input");
+
+    expect(onSearch).not.toBeCalled();
+
+    act(() => {
+        inputField.dispatchEvent(
+            new KeyboardEvent("keydown", { bubbles: true, key: "Enter" })
+        );
+    });
+
+    expect(onSearch).toBeCalledTimes(1);
+});
+
+it("selects all text in the input field when focused", () => {
+    act(() => {
+        render(<SearchBar />, root);
+    });
+    const searchBar = grabSearchBar();
+    const inputField = searchBar.querySelector("input");
+
+    expect(inputField.selectionStart).toBe(0)
+    expect(inputField.selectionEnd).toBe(0)
+
+    const searchString = "test if this is selected"
+    act(() => {
+        inputField.value = searchString
+        inputField.focus()
+    });
+
+    expect(inputField.selectionStart).toBe(0)
+    expect(inputField.selectionEnd).toBe(searchString.length)
+})
