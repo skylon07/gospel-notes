65566b47e14b52244a3b7c58b0bc84f192a3b0db
David M. Brown <david@davtar.org>
2021-03-25 23:33:34 -0600

    Adds an onClick test
    This increases coverage to 100% for DropMenu.test yay

--- src/navigation/DropMenu.test.js
+++ src/navigation/DropMenu.test.js
@@ -3,7 +3,31 @@ import ReactDOM from "react-dom";
 
 import DropMenu from "./DropMenu.js";
 
+import { render, fireEvent, screen } from "@testing-library/react";
+
 it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<DropMenu hidden />, div);
+});
+
+it("calls onClick when clicked", () => {
+    var clicked = false;
+    function handleClick() {
+        clicked = true;
+    }
+    const div = document.createElement("div");
+    render(
+        <DropMenu
+            hidden
+            ariaLabel="test-dropmenu"
+            onClick={() => handleClick()}
+        />,
+        div
+    );
+
+    // Click the main menu button
+    //fireEvent.click(screen.getByRole("button", { label: /main-menu-button/i }));
+    fireEvent.click(screen.getByLabelText("test-dropmenu"));
+
+    expect(clicked).toBeTrue();
 });
