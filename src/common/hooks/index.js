2c067277e64e9042d7e66ace675a3f62e0fd609a
skylon07 <taylor@davtar.org>
2021-05-29 09:20:55 -0400

    Export useClass in common/hooks

--- src/common/hooks/index.js
+++ src/common/hooks/index.js
@@ -1,4 +1,5 @@
 import useStaticValue from "./useStaticValue.js"
 import useMountedState from "./useMountedState.js"
+import useClass from "./useClass.js"
 
-export { useStaticValue, useMountedState }
+export { useStaticValue, useMountedState, useClass }
