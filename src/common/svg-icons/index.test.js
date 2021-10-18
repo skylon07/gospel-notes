96fe7d133491560f20c93a6140366d43c17196ce
Taylor Brown <taylor@davtar.org>
2021-10-18 09:44:14 -0600

    Overhaul svg-icons tests
    Changes include the standard implementation of test-utils, as well as
    using "screen" instead of "grab..." functions.
    
    On top of that, the mock of SVGIcon.propTypes was removed (seemed like
    bad practice...) and replaced with (a better version of) the
    console.error mock, called "ignorePropTypesWarnings()".

--- src/common/svg-icons/index.test.js
+++ src/common/svg-icons/index.test.js
@@ -1,58 +1,40 @@
 import React from "react"
-import { render, unmountComponentAtNode } from "react-dom"
-import { act } from "react-dom/test-utils"
+import { render, cleanup, screen } from "common/test-utils"
 
 import { SVGIcon } from "."
 
-let root = null
 const origError = console.error
-beforeEach(() => {
-    root = document.createElement("div")
-    document.body.appendChild(root)
-
-    // mock console to ignore prop types errors
+function ignorePropTypesWarnings() {
     console.error = (...errors) => {
-        if (!(errors[0] + "").includes("Warning: Failed prop type")) {
+        const isPropTypeWarning = /Warning: Failed.*type:.*Invalid prop/
+        if (!isPropTypeWarning.test(errors + "")) {
             origError.call(console, ...errors)
         }
     }
-})
+}
+
 afterEach(() => {
-    unmountComponentAtNode(root)
-    document.body.removeChild(root)
-    root = null
-
-    // reset mock
+    // reset possible console.error() override
     console.error = origError
+    
+    cleanup()
 })
-
-function grabSVGIcon() {
-    return document.querySelector("[data-testid='svg-icon']")
-}
-
-function grabSVGFrom(icon) {
-    return icon.querySelector("svg")
-}
 
 it("renders without crashing", () => {
-    render(<SVGIcon />, root)
+    render(<SVGIcon />)
 })
 
 it("renders with a CSS class", () => {
-    act(() => {
-        render(<SVGIcon />, root)
-    })
-    const icon = grabSVGIcon()
+    render(<SVGIcon />)
+    const icon = screen.getByLabelText(/icon/)
 
     expect(icon).toHaveClass("SVGIcon")
 })
 
 it("renders a blank icon by default", () => {
-    act(() => {
-        render(<SVGIcon />, root)
-    })
-    const icon = grabSVGIcon()
-    const svg = grabSVGFrom(icon)
+    render(<SVGIcon />)
+    const icon = screen.getByLabelText(/icon/)
+    const svg = icon.querySelector("svg")
     const children = [...svg.childNodes]
 
     // this assumes a blank icon is an SVG with no children
@@ -61,14 +43,11 @@ it("renders a blank icon by default", () => {
 
 it("renders an icon when provided with a type", () => {
     const type = "burger"
-    act(() => {
-        render(<SVGIcon type={type} />, root)
-    })
-    const icon = grabSVGIcon()
-    const svg = grabSVGFrom(icon)
+    render(<SVGIcon type={type} />)
+    const icon = screen.getByLabelText(/icon/)
+    const svg = icon.querySelector("svg")
 
-    // this also tests that the bars/burger icon consists of three lines;
-    // that is less important than the fact it simply has an icon
+    // this assumes that the bars/burger icon consists of three lines
     expect(svg.children.length).toBe(3)
     for (const child of svg.children) {
         expect(child.tagName.toLowerCase()).toBe("line")
@@ -78,24 +57,19 @@ it("renders an icon when provided with a type", () => {
 describe("CSS class tests", () => {
     it("renders the icon type as a class name", () => {
         const type = "burger"
-        act(() => {
-            render(<SVGIcon type={type} />, root)
-        })
-        const icon = grabSVGIcon()
+        render(<SVGIcon type={type} />)
+        const icon = screen.getByLabelText(/icon/)
 
         expect(icon).toHaveClass(type)
     })
 
     it("renders the 'invalid' class name when an invalid type is given", () => {
-        const type = "there is no way this is a valid type"
-        act(() => {
-            // ignore the prop types warning... I'm aware I'm doing something bad
-            const origTypes = SVGIcon.propTypes
-            SVGIcon.propTypes = {}
-            render(<SVGIcon type={type} />, root)
-            SVGIcon.propTypes = origTypes
-        })
-        const icon = grabSVGIcon()
+        const type = "!_ there . is no-way this is a v@lid type"
+        // ignore the prop types warning...
+        // (I'm aware this is "bad", React)
+        ignorePropTypesWarnings()
+        render(<SVGIcon type={type} />)
+        const icon = screen.getByLabelText(/icon/)
 
         expect(icon).toHaveClass("invalid")
     })
