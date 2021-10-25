d8ddb879e4fc8089f6f13ecfb928ef592edc39cd
Taylor Brown <taylor@davtar.org>
2021-10-24 22:56:44 -0600

    Conform BetaDisclaimer tests to use common/test-utils

--- src/MainApp/BetaDisclaimer.test.js
+++ src/MainApp/BetaDisclaimer.test.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { render } from "react-dom"
-// import { act } from "react-dom/test-utils";
+import { render, cleanup } from "common/test-utils"
 import BetaDisclaimer from "./BetaDisclaimer.js"
 
 it("renders without crashing", () => {
-    const div = document.createElement("div")
-    render(<BetaDisclaimer hidden />, div)
+    render(<BetaDisclaimer hidden />)
+    cleanup()
 })
 
 // not many tests needed for this component
